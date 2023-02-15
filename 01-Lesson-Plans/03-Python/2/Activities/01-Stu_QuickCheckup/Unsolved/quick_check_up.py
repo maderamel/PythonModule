f3a0612cd8e61aa3749558ed97d9513f78c43f7f
Maderamel <maderam96@gmail.com>
2023-02-15 10:42:37 -0800

    completed activity quick check up

--- 01-Lesson-Plans/03-Python/2/Activities/01-Stu_QuickCheckup/Unsolved/quick_check_up.py
+++ 01-Lesson-Plans/03-Python/2/Activities/01-Stu_QuickCheckup/Unsolved/quick_check_up.py
@@ -1,13 +1,19 @@
 # Print Hello User!
-
+print("Hello user!")
 
 # Take in User Input
-
+userName = input("What is your name?")
 
 # Respond Back with User Input
-
+print("Hello "+ userName)
 
 # Take in the User Favorite Number
-
+favNumber = int(input("What is your favorite number?"))
 
 # Respond Back with a statement based on your favorite number
+if favNumber > 5:
+    print("Your favorite number is greater than mine.")
+elif favNumber < 5:
+    print("Your favorite number is less than mine.")
+else:
+    print("Your favorite color is the same as mine!")   
